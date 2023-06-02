44f7139ab04b0d3c6df263b54cc758c83a2d9f98
alvaroBetter <124126969+alvaroBetter@users.noreply.github.com>
2023-06-02 11:12:33 +0200

    Fixed witdh

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -19,11 +19,11 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
 	return (
 		<form
 			onSubmit={handleSubmit}
-			className="flex justify-center w-screen max-w-1x1"
+			className="flex justify-center w-screen w-2/4"
 			action="">
 			<input
 				type="text"
-				className="w-1/4 text-black border border-gray-300 rounded-l py-2 px-4 w-full  focus:border-blue-500"
+				className=" text-black border border-gray-300 rounded-l py-2 px-4 w-full  focus:border-blue-500"
 				value={query}
 				onChange={handleChange}
 				placeholder="Search for a movie"
